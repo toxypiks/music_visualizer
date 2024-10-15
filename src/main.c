74f98cbe739623aeb88e7535b839d3769f722a84
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-15 16:09:01 +0200

    Print information about music sample rate and size and channels

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <raylib.h>
 
+void callback (void *bufferData, unsigned int frames)
+{
+  (void) bufferData;
+  // printf("callback(frames = %u)\n", frames);
+}
 
 int main(void)
 {
@@ -10,10 +16,25 @@ int main(void)
 
   InitAudioDevice();
   Music music = LoadMusicStream("../sounds/crystalcastles_air_war.mp3");
+  printf("music.frameCount = %u\n", music.frameCount);
+  printf("music.stream.sampleRate = %u\n", music.stream.sampleRate);
+  printf("music.stream.sampleSize = %u\n", music.stream.sampleSize);
+  printf("music.stream.channels = %u\n", music.stream.channels);
+
   PlayMusicStream(music);
+
+  AttachAudioStreamProcessor(music.stream, callback);
 
   while(!WindowShouldClose()) {
 	UpdateMusicStream(music);
+
+	if(IsKeyPressed(KEY_SPACE)) {
+	  if(IsMusicStreamPlaying(music)) {
+		PauseMusicStream(music);
+	  } else {
+		ResumeMusicStream(music);
+	  }
+	}
 
 	BeginDrawing();
 	ClearBackground(BLACK);
