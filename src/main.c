ab1a42dc09e3fad079714933adc3f1bc3679b97a
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-15 15:11:03 +0200

    Initialize window which plays sound

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <raylib.h>
 
-int main() {
-	printf("Hello World\n");
-	return 0;
+
+int main(void)
+{
+  InitWindow(800, 600, "Musializer");
+  SetTargetFPS(60);
+
+  InitAudioDevice();
+  Music music = LoadMusicStream("../sounds/crystalcastles_air_war.mp3");
+  PlayMusicStream(music);
+
+  while(!WindowShouldClose()) {
+	UpdateMusicStream(music);
+
+	BeginDrawing();
+	ClearBackground(BLACK);
+	EndDrawing();
+  }
+
+  return 0;
 }
