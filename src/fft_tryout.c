49736ed7adbfffc7a4c895e3017cb9d78175be13
toxypiks <HannaHinkelbein89@gmx.de>
2024-10-20 14:51:20 +0200

    Implement easy fourier transformation

--- src/fft_tryout.c
+++ src/fft_tryout.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
+float pi;
+
 int main (void)
 {
-  double pi = atan2f(1, 1)*4; //vector of 45 degree mulitplied by 4
-  printf("%f\n", pi);
+  pi = atan2f(1, 1)*4; //vector of 45 degree mulitplied by 4
 
   size_t n = 8;
   float in[n];
+  float out[n];
 
   for(size_t i = 0; i < n; ++i) {
 	float t = (float)i/n;
-	in[i] = sinf(2*pi*t);
+	in[i] = sinf(2*pi*t*1) + sinf(2*pi*t*3);
   }
+
+  for(size_t f = 0; f < n; ++f) {
+	out[f] = 0;
+	for (size_t i = 0; i < n; ++i) {
+	  float t = (float)i/n;
+	  out[f] += in[i]*sinf(2*pi*f*t);
+	}
+  }
+
+  for (size_t f = 0; f < n; ++f) {
+	printf("%02zu: %.2f\n", f, out[f]);
+  }
+
+  /*for (size_t i = 0; i < n; ++i) {
+	printf("%f\n", in[i]);
+	}*/
   return 0;
 }
